a3019db176a6e54a1300bba5299d83a50656d3cf
Gál Ádám <galadam96@gmail.com>
2023-09-08 15:10:45 +0200

    Bug fixes and finished game mechanics
    //FIXED BUG: New Game Choice: Once you type y or n, you have to do again, it does not recognize the first input. Maybe buffer issue.
    //FIXED BUG: Not all rows and columns checks right for winner
    //FIXED BUG: When typed a letter infinite loop occured because of invalid scanf_s return value.
    //ADDED: Diagonal check for winner
    //ADDED: When no more steps is possible, give DRAW text and ask for a new game
    //REMOVED changePlayerTurn function, as updateBoard handles it via pointers
    //CHANGED: Valid input check is moved inside getInput function, and removed from UpdateBoard. Checks for valid numbers and clears input buffers on invalid entries.

--- X_ticTacToe/ticTacToe.c
+++ X_ticTacToe/ticTacToe.c
@@ -1,9 +1,6 @@
 //Date: 2023.08.18
 //Purpose: TicTacToe game for 2 local players in console
-//BUGS: Line 75 New Game Choice: Once you type y or n, you have to do again, it does not recognize the first input. Maybe buffer issue.
-//BUGS: Not all rows and columns checks right for winner
-//TBD: Diagonal check for winner
-//TBD: When no more steps is possible, give DRAW text and ask for a new game.
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,14 +8,8 @@
 /*Function prototypes*/
 void showBoard(char board[3][3]);
 void updateBoard(char board[3][3], int input, int *playerTurn);
-int getInput(void);
-void changePlayerTurn(int *turn);
+int getInput(char board[3][3]);
 char checkWinner(char board[3][3]);
-
-/*Global variables*/
-
-int showErrorMessageOccupied = 0;
-int showErrorMessageValidNum = 0;
 
 /*Main game loop*/
 int main() {
@@ -34,7 +25,7 @@ int main() {
 		{'7','8','9'},
 	};
 
-	while (1) {
+	while (isNewGame != 'n') {
 
 		while (gameRunning == 1) {
 			system("cls"); // Clears the console
@@ -43,49 +34,39 @@ int main() {
 			showBoard(board);
 			printf("\nPlayer %i, enter a number: ", playerTurn);
 
-
 			winner = checkWinner(board);
 			if (winner != ' ') {
 				if (winner == 'X') {
-					//system("cls");
-					//showBoard(board);
 					printf("Winner is X!");
 					gameRunning = 0;
 					break;
 				}
 				else if (winner == 'O') {
-					//system("cls");
-					//showBoard(board);
 					printf("Winner is O!");
+					gameRunning = 0;
+					break;
+				}
+				else if (winner == 'D') {
+					printf("DRAW!");
 					gameRunning = 0;
 					break;
 				}
 			}
 
 			//printf("\nDEBUG: input is:%i \n", input);
-			//Gives error message for invalid numbers. Valid numbers: 1-9
-			if (showErrorMessageValidNum == 1)
-			{
-				printf("\nInvalid number. Choose from 1 to 9!\n");;
-				showErrorMessageValidNum = 0;
-			}
 
-			if (showErrorMessageOccupied == 1) {
-				printf("\nThis space is already occupied. Choose another one!\n");
-				showErrorMessageOccupied = 0;
-			}
-			input = getInput();
+			input = getInput(board);
 			updateBoard(board, input, &playerTurn);
 			
 		}
 
 		/*New Game choice*/
 		//!!BUG: Once you type y or n, you have to do again, it does not recognize the first input. Maybe buffer issue.
+		//!FIX: SPACE before %c solved the issue. Clears the residue of previous input.
 		while (gameRunning == 0) {
 			printf("\n Do you want to start a new game? [y/n] ");
-			scanf_s("%c", &isNewGame);
-			//isNewGame = getchar(); // Gets 1 character input
-			//getchar(); // Consumes the newline character from the buffer
+			scanf_s(" %c", &isNewGame);
+	
 			// Clears the buffer from residual space and enters from previous input
 			int c;
 			while ((c = getchar()) != '\n' && c != EOF);
@@ -98,9 +79,8 @@ int main() {
 				input = 1;
 				winner = ' ';
 				isNewGame = ' ';
-				
+					
 				// Reset board
-
 				for (int i = 0; i < 3; i++) {
 					for (int j = 0; j < 3; j++) {
 						board[i][j] = '1' + i * 3 + j;
@@ -146,87 +126,128 @@ void showBoard(char board[3][3]) {
 
 
 }
-//updateBoard handles playerTurn change by pointers simply.
-/*void changePlayerTurn(int* turn) {
-	//Return which player shall go next.
-	if (*turn == 1) {
-		*turn = 2;
-	}
-	else if (*turn == 2) {
-		*turn = 1;
-	}
-	return;
-}*/
 
-int getInput(void) {
-	//Gets input from user
-	int input;
-	scanf_s("%i", &input);
+
+int getInput(char board[3][3]) {
+	//Gets input from user and validates it
+	int input = 0;
+	int result;
+	int row, col;
+	
+	while (1) {
+
+		result = scanf_s("%i", &input); // Store scanf_s return value (1 if valid parameter specifier is given)
+		//Invalid input (not number)
+		if (result != 1) {
+			printf("\nInvalid input. Only numbers from 1 to 9!\n");
+			// Clear input buffer 
+			int c;
+			while ((c = getchar()) != '\n' && c != EOF);
+		}
+
+		//Invalid number (not 1-9)
+		else if (input < 1 || input > 9) {
+			printf("\nInvalid number. Choose from 1 to 9!\n");
+			// Clear input buffer 
+			int c;
+			while ((c = getchar()) != '\n' && c != EOF);
+		}
+
+		//Valid input
+		else {
+			row = (input - 1) / 3;
+			col = (input - 1) % 3;
+
+			//Occupied space already
+			if (board[row][col] == 'O' || board[row][col] == 'X') {
+			printf("\nThis space is already occupied. Choose another one!\n");
+			}
+
+			//Valid input, not occupied
+			else {
+				break;
+			}
+		}
+	}
+	
 	return input;
 }
 
 void updateBoard(char board[3][3], int input, int *playerTurn) {
 	//Updates the board based on player input with X and O-s.
 	//PASS BY REFERENCE: modifies the array by giving the address of the variable and not the value.
-	//! Look it UP: what is the diff between this and changePlayerTurn. Here I do not return anything.
-	//! Ans: The array in C gets passed as a memory address by default. To to do the same with a variable: use pointers
+	//The array in C gets passed as a memory address by default. To to do the same with a variable: use pointers
 
 	int row = (input - 1) / 3;
 	int col = (input - 1) % 3;
 
-	if (input < 1 || input > 9) {
-		showErrorMessageValidNum = 1;
-	}
-
-	else if (board[row][col] == 'O' || board[row][col] == 'X') {
-		showErrorMessageOccupied = 1; //Check for already occupied spaces, give error message
-	}
-	else if (*playerTurn == 1) {
+	if (*playerTurn == 1) {
 		board[row][col] = 'X';
-		//changePlayerTurn(playerTurn);
 		*playerTurn = 2;
 	}
 	else if (*playerTurn == 2) {
 		board[row][col] = 'O';
-		//changePlayerTurn(playerTurn);
 		*playerTurn = 1;
 	}
 	
 	return;
 }
 
-
 char checkWinner(char board[3][3]) {
-	//!!!!BUG: Only works for 1st row
 	//Check if somebody won. Return winner char or D for draw
 	char winner = ' ';
+	int totalMoves = 0;
 
-	//check Rows
+	// Checks for diagonals 
+
+	if (board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X') {
+		winner = 'X';
+	}
+	else if (board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X') {
+		winner = 'X';
+	}
+
+	else if (board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O') {
+		winner = 'O';
+	}
+
+	else if (board[0][2] == 'O' && board[1][1] == 'O' && board[2][0] == 'O') {
+		winner = 'O';
+	}
+
+	// Check for rows and columns
+
 	for (int i = 0; i < 3; i++) {
-		if(board[i][0] == board[i][1] && board[i][0] == board[i][2]){
-			if (board[i][0] == 'X') {
-				winner = 'X';
-				break;
-			}
-			if (board[i][0] == 'O') {
-				winner = 'O';
-				break;
-			}
 
+		//Rows
+		if (board[i][0] == 'X' && board[i][1] == 'X' && board[i][2] == 'X') {
+			winner = 'X';
+		}
+		else if (board[i][0] == 'O' && board[i][1] == 'O' && board[i][2] == 'O') {
+			winner = 'O';
+		}
+
+		//Columns
+		else if (board[0][i] == 'X' && board[1][i] == 'X' && board[2][i] == 'X') {
+			winner = 'X';
+		}
+
+		else if (board[0][i] == 'O' && board[1][i] == 'O' && board[2][i] == 'O') {
+			winner = 'O';
 		}
 	}
 
-	for (int j = 0; j < 3; j++) {
-		if (board[0][j] == board[1][j] && board[0][j] == board[2][j]) {
-			if (board[j][0] == 'X') {
-				winner = 'X';
-				break;
-			}
-			if (board[j][0] == 'O') {
-				winner = 'O';
-				break;
+	//Check for draw 
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			if (board[i][j] == 'X' || board[i][j] == 'O') {
+				totalMoves++;
 			}
 		}
+	}
+
+	if (totalMoves == 9) {
+		winner = 'D';
 	}
 
 	return winner;
